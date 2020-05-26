a74a955f886aab844854a2605fbd989243775bd4
Ahmed Shaalan <ahmed_shaalan@icloud.com>
2020-05-26 11:55:29 +0200

    Added: UART library

--- src/main.c
+++ src/main.c
@@ -11,6 +11,17 @@ int main(void) {
 
     /* Initialize LCD 					*/
     lcd_init();
+    _delay_ms(1000);
+    lcd_cursor_home();
+    lcd_clear_display();
+
+    // initialize UART to read only
+    // wait for instruction
+    // check if the instruction is command?
+    // - if command, wait for the command
+    // - excute command
+    // if not instruction
+    // - print
 
     while (1) {
     }
